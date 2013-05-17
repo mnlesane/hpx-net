df8f69c0a079ccbab44391b89a31b72e356d075d
michael lesane <michael@thinkbig.home>
2013-05-17 11:16:38 -0500

    Restructured network, genetic algorithm implementation (XOR problem) added.

--- net.cpp
+++ net.cpp
@@ -18,33 +18,6 @@ float df(float x)
 {
 	return 1.0-pow(x,2.0);
 }
-float error(float target, float result)
-{
-	return 0.5*pow(target-result,2.0);
-}
-float derror(float target,float result)
-{
-	return result-target;
-}
-int int_in_vector(std::vector<int> v, int f)
-{
-	for(int i = 0; i < v.size(); i++)
-	{
-		//std::cout << f << " in " << v[i] << "?\n";
-		if(v[i]==f) return i;
-	}
-	return -1;
-}
-std::string implode(std::string delimiter, std::vector<std::string> array){
-	std::string out = "";
-	for(int i = 0; i < array.size(); i++)
-	{
-		out.append(array[i]);
-		if(i<array.size()-1) out.append(delimiter);
-	}
-	return out;
-}
-
 void init()
 {
 	unsigned int seed;
@@ -54,228 +27,249 @@ void init()
 	srand(seed);
 }
 
-class neuron
-{
-	public:
-
-	std::vector<int> activation_ids;
-	std::vector<float> roots;
-
-	std::vector<float> weights;
-	std::vector<float> last_change;
-
-	float value, row, bias;
-
-	int id;
-
-	neuron(float row, float bias, int id)
-	{
-		this->value = 0.0;
-		this->row = row;
-		this->bias = bias;
-		if(bias) this->value = 1.0;
-		this->id = id;
-	}
-};
-
 float productsum(std::vector<float> roots, std::vector<float> weights)
 {
 	float out = 0;
 	for(int i = 0; i < roots.size(); i++)
 	{
 		out += roots[i]*weights[i];
-		//std::cout << roots[i] << " x " << weights[i] << "\n";
 	}
 	return out;
 }
 
+class neuron
+{
+	public:
+
+	std::vector<float> weights;
+	std::vector<float> last_change;
+
+	float value, bias, delta;
+
+	neuron(float row, float bias);
+
+	void run(std::vector<neuron> roots);
+};
+
+std::vector<float> extract_roots(std::vector<neuron> contents)
+{
+
+	std::vector<float> result;
+	for(int i = 0; i < contents.size(); i++)
+		result.push_back(contents[i].value);
+	return result;
+}
+
+	neuron::neuron(float row, float bias)
+	{
+		this->value = 0.0;
+		this->bias = bias;
+		if(bias) this->value = 1.0;
+	}
+	void neuron::run(std::vector<neuron> roots)
+	{
+		if(this->bias == 1) return;
+		this->value = f(productsum(extract_roots(roots),this->weights));
+	}
+
+class neuron_row
+{
+	public:
+
+	std::vector<neuron> contents;
+
+	neuron_row(std::vector<neuron> init)
+	{
+		this->contents = init;
+	}
+
+	void run(std::vector<neuron> roots)
+	{
+		for(int i = 0; i < this->contents.size(); i++)
+		{
+			this->contents[i].run(roots);
+		}
+	}
+
+	void add(neuron x)
+	{
+		contents.push_back(x);
+	}
+
+	int size()
+	{
+		return this->contents.size();
+	}
+};
+
 class network
 {
 	public:
-	std::vector<neuron> net;
+	std::vector<neuron_row> rows;
 
 	void setSensors(float vals[])
 	{
 		for(int i = 0; i < sizeof(vals)/sizeof(float); i++)
-		{
-			net[i].value = vals[i];
-		}
+			this->rows[0].contents[i].value = vals[i];
 	}
 	void setSensors(std::vector<float> vals)
 	{
 		for(int i = 0; i < vals.size(); i++)
-		{
-			net[i].value = vals[i];
-		}
+			this->rows[0].contents[i].value = vals[i];
 	}
 	void run() //Forward Propagation
 	{
-
-		float ids[net.size()]; //substitute this with a future array/vector later
-
-		for(int it = 0; it <= net[net.size()-1].row; it++)
+		for(int i = 1; i < this->rows.size(); i++)
 		{
-			std::vector<float> roots;
-			for(int i = 0; i < net.size(); i++) //Learner node
-			{
-				if(net[i].row==it-1)
-				{
-					roots.push_back(net[i].value);	
-					continue;
-				}			
-				if(net[i].bias==1||net[i].row==0||(net[i].row!=it)) continue;
-				ids[i] = productsum(roots,net[i].weights); //future goes here
-			}
-			for(int i = 0; i < net.size(); i++)
-			{
-				if(net[i].bias==1||net[i].row==0||net[i].row!=it) continue;
-				net[i].value = ids[i]; //wait goes here
-				net[i].value = f(net[i].value);
-			}
+			this->rows[i].run(this->rows[i-1].contents);
 		}
 	}
-
 	std::vector<float> reverse(std::vector<float> vals)
 	{
 		std::vector<float> result;
 		for(int i = vals.size()-1; i >= 0; i--)
-		{
 			result.push_back(vals[i]);
-		}
 		return result;
 	}
-
-	float correct_serial(std::vector<float> v,float m,float n)
+	float correct_serial(std::vector<float> v, float m /*learning_rate*/, float n /*momentum*/)
 	{
 		std::vector<float> vi = v;
-		v = this->reverse(v);
-		float deltas[net.size()];
+		//v = this->reverse(v);
 
 		float error;
 
-		for(int i = net.size()-1; i >= 0; i--)
+		for(int i = this->rows.size()-1; i >= 1; i--)
 		{
-			if(net[i].row == 0 || net[i].bias == 1) continue;
-			if(net[i].row == net[net.size()-1].row) //output deltas
+			for(int j = 0; j < this->rows[i].size(); j++)
 			{
-				float target = v[0];
-				v.erase(v.begin(),v.begin()+1);
-
-				error = target - net[i].value;
-				float dfunc = df(net[i].value);
-				deltas[i] = error*dfunc;
-
-				for(int j = 0; j < net[i].weights.size(); j++) //previous layer
+				if(this->rows[i].contents[j].bias == 1) continue;
+				if(i == this->rows.size()-1) //output deltas
 				{
-					int pos = net[i].activation_ids[j];
-					float change = deltas[i]*net[pos].value;
-					float change2 = m*change + n*net[i].last_change[j];
-					net[i].weights[j] += change2;
-					net[i].last_change[j] = change;
-				}
-			}	
-			else //hidden deltas
-			{
-				error = 0;
-				for(int j = 0; j < net.size(); j++)
-				{
-					int pos = int_in_vector(net[j].activation_ids,i);
-					if(pos >= 0)
+					float target = v[0];
+					v.erase(v.begin(),v.begin()+1);
+
+					error = target - this->rows[i].contents[j].value;
+
+					float dfunc = df(this->rows[i].contents[j].value);
+					this->rows[i].contents[j].delta = error*dfunc;
+
+					for(int k = 0; k < this->rows[i-1].size(); k++) //previous layer
 					{
-						error += deltas[j]*net[j].weights[pos];
+						float change = this->rows[i].contents[j].delta*this->rows[i-1].contents[k].value;
+						float change2 = m*change + n*this->rows[i].contents[j].last_change[k];
+
+						this->rows[i].contents[j].weights[k] += change2;
+						this->rows[i].contents[j].last_change[k] = change;
 					}
 				}
-				deltas[i] = error*df(net[i].value);
-
-				for(int j = 0; j < net[i].weights.size(); j++)
+				else //hidden deltas
 				{
-					neuron val = net[j];
-					float change = deltas[i]*net[j].value;
-					int pos = net[i].activation_ids[j];
+					error = 0;
+					for(int k = 0; k < this->rows[i+1].size(); k++)
+					{
+						error += this->rows[i+1].contents[k].delta * this->rows[i+1].contents[k].weights[j];
+					}
+					this->rows[i].contents[j].delta = error*df(this->rows[i].contents[j].value);
 
-					float change2 = m*change + n*net[i].last_change[j];
-					net[i].weights[j] += change2;
-					net[i].last_change[j] = change;
+					for(int k = 0; k < this->rows[i].contents[j].weights.size(); k++) //previous layer
+					{
+						float change = this->rows[i].contents[j].delta * this->rows[i-1].contents[k].value;
+						float change2 = m*change + n*this->rows[i].contents[j].last_change[k];
+
+						this->rows[i].contents[j].weights[k] += change2;
+						this->rows[i].contents[j].last_change[k] = change;
+					}
 				}
 			}
 		}
 		error = 0;
 		float out_index = 0;
 
-		for(int i = 0; i < net.size(); i++)
+		for(int i = 0; i < this->rows[this->rows.size()-1].size(); i++)
 		{
-			if(net[i].row==net[net.size()-1].row)
-			{
-				error += 0.5*pow(vi[out_index]-net[i].value,2);
-				out_index++;
-			}
+			error += 0.5*pow(vi[out_index]-this->rows[this->rows.size()-1].contents[i].value,2);
+			out_index++;
 		}
 		return error;
 	}
-	network(int in, int hidden_rows, int hidden_cols, int out, int bias = 0)
+	network()
 	{
+	}
+	void init(int in, int hidden_rows, int hidden_cols, int out, int bias = 0)
+	{
+		std::vector<neuron_row> row;
+
+		std::vector<neuron> input_layer;
+
 		int rowcount = 0;
-		std::vector<neuron> net;
-		int netcount = 0;
 
 		for(int i = 0; i < in; i++)
 		{
-			net.push_back(neuron(rowcount,0,netcount));
-			netcount++;
+			input_layer.push_back(neuron(rowcount,0));
 		}
-		net.push_back(neuron(0,1,netcount));
+		input_layer.push_back(neuron(0,1));
 		rowcount++;
-		netcount++;
+
+		row.push_back(neuron_row(input_layer));
 
 		for(int i = 0; i < hidden_rows; i++) //"Rows" in Hidden Layers
 		{
+			std::vector<neuron> hidden_layer;
 			for(int j = 0; j < hidden_cols; j++) //"Columns" in Hidden Layers
 			{
 				//Nodes for hidden layers
-				net.push_back(neuron(rowcount,0,netcount));
-				for(int k = 0; k < net.size(); k++)
+				hidden_layer.push_back(neuron(rowcount,0));
+				for(int k = 0; k < row[i].size(); k++)
 				{
-					if(net[k].row == rowcount-1)
-					{
-						net[netcount].activation_ids.push_back(k);
-//						net[netcount].weights.push_back(0);//rnd()-0.5);
-						net[netcount].weights.push_back(rnd()-0.5);
-						net[netcount].last_change.push_back(0);
-					}
+					hidden_layer[j].weights.push_back(rnd()-0.5);
+					hidden_layer[j].last_change.push_back(0);
 				}
-				netcount++;
 			}
 
 			//Bias node for each hidden layer
 			if(bias == 1)
 			{
-				net.push_back(neuron(rowcount,1,netcount));
-				netcount++;
+				hidden_layer.push_back(neuron(rowcount,1));
 			}
 
 			rowcount++;
+			row.push_back(neuron_row(hidden_layer));
 		}
 
 		//Output node/layer
+		std::vector<neuron> output_layer;
 		for(int i = 0; i < out; i++)
 		{
-			net.push_back(neuron(rowcount,0,netcount));
-			for(int k = 0; k < net.size(); k++)
+			output_layer.push_back(neuron(rowcount,0));
+			for(int k = 0; k < hidden_cols + 1; k++)
 			{
-				if(net[k].row == rowcount-1)
+				output_layer[i].weights.push_back(rnd()-0.5);
+				output_layer[i].last_change.push_back(0);
+			}
+		}
+		row.push_back(neuron_row(output_layer));
+		this->rows = row;
+	}
+	network(int in, int hidden_rows, int hidden_cols, int out, int bias = 0)
+	{
+		this->init(in,hidden_rows,hidden_cols,out,bias);
+	}
+	void profile()
+	{
+		std::cout << "\n";
+		for(int i = 0; i < this->rows.size(); i++)
+		{
+			std::cout << "ROW " << i << "\n";
+			for(int j = 0; j < this->rows[i].contents.size(); j++)
+			{
+				std::cout << "\tCOL " << j << " -> VAL " << this->rows[i].contents[j].value << "\n";
+				for(int k = 0; k < this->rows[i].contents[j].weights.size(); k++)
 				{
-					net[netcount].activation_ids.push_back(k);
-//					net[netcount].weights.push_back(0);//rnd()-0.5);
-					net[netcount].weights.push_back(rnd()-0.5);
-					net[netcount].last_change.push_back(0);
+					std::cout << "\t\tWEIGHT " << k << ": " << this->rows[i].contents[j].weights[k] << "\n";
 				}
 			}
-			netcount++;
 		}
-		this->net = net;
 	}
-/*
-*/
 };
 
 std::vector<float> to_vector(float x[],int s)
@@ -287,15 +281,7 @@ std::vector<float> to_vector(float x[],int s)
 /*
 int main()
 {
-	network n(20,200,200,1,1);
-
-	for(int i = 0; i < 100; i++)
-	  {
-	    std::cout << i << "\n";
-	    n.run();
-	  }
-
-	return 0;
+	network n(2,1,2,1,1);
 
 //XOR
 	float tests[][2] =
@@ -313,7 +299,7 @@ int main()
 		{0.0}
 	};
 
-	for(int i = 0; i < 4000; i++)
+	for(int i = 0; i < 5000; i++)
 	{
 		std::cout << i << " ";
 		int s = i%(sizeof(tests)/sizeof(tests[0]));
@@ -334,18 +320,15 @@ int main()
 
 		int valid = 1;
 
-		for(int it = 0; it < n.net.size(); it++)
+		for(int it = 0; it < n.rows[n.rows.size()-1].contents.size(); it++)
 		{
 			int counter = 0;
-			if(n.net[it].row == n.net[n.net.size()-1].row)
-			{
-				float r = n.net[it].value;
-				if(r < 0) r = 0;
-				r = round(r);
-				std::cout << r << " ";
-				if(round(n.net[it].value) != targets[s][counter]) valid = 0;
-				else counter++;
-			}
+			float r = n.rows[n.rows.size()-1].contents[it].value;
+			if(r < 0) r = 0;
+			r = round(r);
+			std::cout << r << " ";
+			if(round(n.rows[n.rows.size()-1].contents[it].value) != targets[s][counter]) valid = 0;
+			else counter++;
 		}
 
 		std::cout << "(";
@@ -363,5 +346,4 @@ int main()
 		std::cout << "\n";
 	}
 	return 0;
-}
-*/
+}*/
